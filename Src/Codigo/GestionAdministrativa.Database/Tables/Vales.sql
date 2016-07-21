997a4693ce92b9439e107745cd3791fe143ed998
silviaeaguilar <silviaeaguilar@gmail.com>
2016-07-21 12:13:46 -0300

    Vales Table

---
+++ Src/Codigo/GestionAdministrativa.Database/Tables/Vales.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Vales]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Numero] INT NOT NULL, 
+    [MovilPresentaId] UNIQUEIDENTIFIER NULL, 
+    [MovilViajeId] UNIQUEIDENTIFIER NULL, 
+    [ClienteId] UNIQUEIDENTIFIER NULL, 
+    [FechaViaje] DATETIME NULL, 
+    [Monto] DECIMAL NULL,     
+    [Presentado] BIT NULL, 
+    [Facturado] BIT NULL, 
+    [Aprobado] BIT NULL,
+	[FechaAlta] DATETIME NULL, 
+    [OperadorAlta] UNIQUEIDENTIFIER NULL, 
+    [SucursalAlta] INT NULL, 
+    [FechaModificacion] DATETIME NULL, 
+    [OperadorModificacion] UNIQUEIDENTIFIER NULL, 
+    [SucursalModificacion] INT NULL, 
+
+)
