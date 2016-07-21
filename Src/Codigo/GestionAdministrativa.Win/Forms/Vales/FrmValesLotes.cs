3a0bbbc64186e2f4f675bee20331d989aa7ed0ed
lindorsito <santiacevedo@gmail.com>
2016-07-21 12:16:52 -0300

    vales lotes

---
+++ Src/Codigo/GestionAdministrativa.Win/Forms/Vales/FrmValesLotes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace GestionAdministrativa.Win.Forms.Vales
+{
+    public partial class FrmValesLotes : Telerik.WinControls.UI.RadForm
+    {
+        public FrmValesLotes()
+        {
+            InitializeComponent();
+        }
+    }
+}
